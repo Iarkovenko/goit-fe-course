61b398a5b5b03b0d26400ca656c050e7d6808fed
Dmytro Iarkovenko <yarkovenko@ukr.net>
2018-05-14 22:30:25 +0300

    diplom project

---
+++ html-css/Diplom Project/smartgrid.js
@@ -0,0 +1,37 @@
+var smartgrid = require('smart-grid');
+ 
+/* It's principal settings in smart grid project */
+var settings = {
+    filename: "_smartgrid",
+    outputStyle: 'scss', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '0px', /* gutter width px || % */
+    mobileFirst: 'mobileFirst', /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '848px', /* max-width оn very large screen */
+        fields: '177px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1200px' /* -> @media (max-width: 1100px) */
+        },
+        sm: {
+            width: '768px'
+             /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '320px'
+        }
+        /* 
+        We can create any quantity of break points.
+ 
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+ 
+smartgrid('./src/precss', settings);
