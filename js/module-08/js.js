a213cdf7708abc808bf25e2eb6a309bcfcf68128
Iarkovenko <yarkovenko@ukr.net>
2018-08-09 00:15:07 +0300

    fixed

--- js/module-08/js.js
+++ js/module-08/js.js
@@ -59,5 +59,4 @@ const galleryItems = [
   { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
   { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
   { preview: 'img/preview-5.jpeg', fullview: 'img/fullview-5.jpeg', alt: "alt text 5" },
-  { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
 ];
